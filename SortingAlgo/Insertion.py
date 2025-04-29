979a90a08af6e9ff7f72d7a9c5e417a3705f8080
Japhet Melquisedec Gonzales <gjaphet999@gmail.com>
2025-04-29 00:30:26 +0000

    yahow

--- SortingAlgo/Insertion.py
+++ SortingAlgo/Insertion.py
@@ -1,3 +1,12 @@
+def insertion_sort(arr):
+    nItems = len(arr)
+    for i in range(1, nItems):
+        j= i
+        while j > 0 and arr[j-1] > arr[j]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+        return arr
+
 def get_median(arr):
     sorted_arr = sorted(arr)
     nItems = len(sorted_arr)
